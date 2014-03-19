cd7d4e70da48ead1ed28941f814d3c570a102c54
Ruben Visser <rubruben@hotmail.com>
2014-03-19 12:00:06 +0100

    Change

---
+++ gen/com/rubenvisser/basicapp/SecondScreen.java
@@ -0,0 +1,24 @@
+package com.rubenvisser.basicapp;
+
+import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SecondScreen extends Activity {
+
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondscreen);
+	
+	}
+}
---
+++ src/com/rubenvisser/basicapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.rubenvisser.basicapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread logoTimer = new Thread(){
+        	public void run(){
+        		try{
+        			sleep(5000);
+        			Intent menuIntent = new Intent("com.rubenvisser.basicapp.MENU");
+        			startActivity(menuIntent);
+        		} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        	
+        		finally{
+        			finish();
+        		}
+        	}
+        };
+        logoTimer.start();
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
